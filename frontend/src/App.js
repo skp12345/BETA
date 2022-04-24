1edcaecaaf2eb8f4f4a72182013f48a50ab15975
m-triple-m <triplem656@gmail.com>
2022-04-24 12:24:06 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,45 +18,57 @@ import Mainhome from "./components/main/home";
 import Login from "./components/main/login";
 import SignUp from "./components/main/signup";
 
-
 function App() {
   return (
     <div>
       <BrowserRouter>
-
         <Routes>
+          <Route element={<Admin></Admin>} path="Admin">
+            <Route
+              element={<Admindashboard></Admindashboard>}
+              path="admindashboard"
+            />
+            <Route element={<Adminfooter></Adminfooter>} path="adminfooter" />
+          </Route>
 
-<Route   element={<Admin></Admin>} path="Admin" >
-  <Route element={<Admindashboard></Admindashboard>} path="admindashboard"/>
-  <Route element={<Adminfooter></Adminfooter>} path="adminfooter"/>
-</Route>
+          <Route element={<Company></Company>} path="Company">
+            <Route element={<Addtest></Addtest>} path="addtest" />
+            <Route
+              element={<Companydashboard></Companydashboard>}
+              path="companydashboard"
+            />
+            <Route
+              element={<Compnayfooter></Compnayfooter>}
+              path="companyfooter"
+            />
+            <Route
+              element={<Companylogin></Companylogin>}
+              path="companylogin"
+            />
+            <Route
+              element={<Companysignup></Companysignup>}
+              path="companysignup"
+            />
+            <Route
+              element={<Companyheader></Companyheader>}
+              path="companyheader"
+            />
+          </Route>
 
+          <Route element={<Main></Main>} path="main">
+            <Route element={<BetaDetails></BetaDetails>} path="betadetails" />
+            <Route element={<Betalisting></Betalisting>} path="betalisting" />
+            <Route element={<Mainhome></Mainhome>} path="Mainhome" />
+            <Route
+              element={<EnrolledTest></EnrolledTest>}
+              path="EnrolledTest"
+            />
+            <Route element={<Login></Login>} path="Login" />
+            <Route element={<SignUp></SignUp>} path="SignUP" />
+          </Route>
 
-<Route element={<Company></Company>} path="Company">
-  <Route element={<Addtest></Addtest>} path="addtest"/>
-  <Route element={<Companydashboard></Companydashboard>} path="companydashboard"/>
- <Route element={<Compnayfooter></Compnayfooter>} path="companyfooter"/>
- <Route element={<Companylogin></Companylogin>} path="companylogin"/>
- <Route element={<Companysignup></Companysignup>} path="companysignup"/>
- <Route element={<Companyheader></Companyheader>} path="companyheader"/>
-</Route>
-
-
-
-<Route element={<Main></Main>} path="main" > 
-<Route element={<BetaDetails></BetaDetails>} path="betadetails"/>
-<Route element={<Betalisting></Betalisting>} path="betalisting"/>
-<Route element={<Mainhome></Mainhome>} path="Mainhome"/>
-<Route element={<EnrolledTest></EnrolledTest>} path="EnrolledTest"/>
-<Route element={<Login></Login>} path="Login"/>
-<Route element={<SignUp></SignUp>} path="SignUP"/>
-
-</Route>
-
-<Route element={<Navigate to="/main/Mainhome" />} path="/" />
-
+          <Route element={<Navigate to="/main/Mainhome" />} path="/" />
         </Routes>
-
       </BrowserRouter>
     </div>
   );
