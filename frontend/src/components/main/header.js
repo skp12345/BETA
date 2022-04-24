dd8eb88729087f8a2d4694c2a7f8aa1c9634d58b
m-triple-m <surajanshraj@gmail.com>
2022-04-24 13:02:48 +0530

    ..

--- frontend/src/components/main/header.js
+++ frontend/src/components/main/header.js
@@ -4,55 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            {/* <nav classNameName="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div classNameName='container'>
-                <a classNameName="navbar-brand" href="#">β-Testing</a>
-                <button classNameName="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span classNameName="navbar-toggler-icon"></span>
-                </button>
-
-                <div classNameName="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul classNameName="navbar-nav mr-auto">
-                        <li classNameName="nav-item active">
-                            <a classNameName="nav-link" href="/">Home <span classNameName="sr-only">(current)</span></a>
-                        </li>
-                        <li classNameName="nav-item">
-                            <a classNameName="nav-link" href="/about.html">About</a>
-                        </li>
-
-                        <li classNameName="nav-item dropdown">
-                            <a classNameName="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Paid Testing
-                            </a>
-                            <div classNameName="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a classNameName="dropdown-item" href="#">PC Software Testing</a>
-                                <a classNameName="dropdown-item" href="#">OS Testing</a>
-                                <a classNameName="dropdown-item" href="#">PC Game Testing</a>
-                                <div classNameName="dropdown-divider"></div>
-                                <a classNameName="dropdown-item" href="#">Android Apk Testinng</a>
-                                <a classNameName="dropdown-item" href="#">Android Game Testing</a>
-                                <a classNameName="dropdown-item" href="#">Write for us</a>
-                            </div>
-                        </li>
-                        <li classNameName="nav-item">
-                            <a classNameName="nav-link" href="/contact.html">Contact Us</a>
-                        </li>
-                        <li classNameName="nav-item">
-                            <a classNameName="nav-link" href="/contact.html">Latest Updates</a>
-                        </li>
-                        <li classNameName="nav-item">
-                            <a classNameName="nav-link disabled" style={{textDecoration: 'underline'}}>Free Testing</a>
-                        </li>
-
-                    </ul>
-                </div>
-                </div>
-            </nav> */}
             
-
-
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">β-Testing</a>
@@ -98,8 +50,6 @@ const Header = () => {
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <NavLink to="/"  className="btn btn-outline-success mx-1" type="submit">Search</NavLink>
-        {/* <button  className="btn btn-outline-danger mx-1" type="submit"> <NavLink to='/main/login'>Login</NavLink> </button> */}
-        {/* <button className="btn btn-outline-danger mx-1" type="submit"><NavLink to='/main/signup'>SignUp</NavLink></button> */}
         <NavLink className="btn btn-outline-danger mx-1" to="/main/login" >Login</NavLink>
         <NavLink className="btn btn-outline-danger mx-1" to="/main/signup" >SignUp</NavLink>
       </form>
