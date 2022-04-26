8c8beffdffeb1a0bbb509f0ca3a1674c3ef3e908
m-triple-m <triplem656@gmail.com>
2022-04-26 14:19:36 +0530

    ..

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../sidebar";
 // import Footer from "./footer";
-import Header from "./header2";
+import Header from "./header3";
 
 const Main = () => {
   return (
